c20a58dbcd5ed606781e03e8ce7fc26941c7ea6f
Tony Christie <tony.christie@gmail.com>
2014-09-29 20:41:54 +0100

    Initial commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Tony Christie on 29/09/2014.
+//  Copyright (c) 2014 Spire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
