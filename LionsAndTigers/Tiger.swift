bf22024e674e646382f74192551fdf0922db4f5b
Tony Christie <tony.christie@gmail.com>
2014-10-09 21:00:16 +0100

    Tigers can chuff!

--- LionsAndTigers/Tiger.swift
+++ LionsAndTigers/Tiger.swift
@@ -14,4 +14,14 @@ struct Tiger {
     var name = ""
     var breed = ""
     var image = UIImage(named:"")
+    
+    func chuff() {
+        println("\(name): Chuff Chuff")
+    }
+    
+    func chuffANumberOfTimes(numberOfTimes: Int) {
+        for var chuff = 0; chuff < numberOfTimes; ++chuff {
+            self.chuff()
+        }
+    }
 }
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
     
+    var myTigers:[Tiger] = []
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +27,9 @@ class ViewController: UIViewController {
         myTiger.breed = "Bengal"
         myTiger.age = 3
         myTiger.image = UIImage(named:"BengalTiger.jpg")
+        
+        myTigers.append(myTiger)
+        myTiger.chuff()
         
 //        println("My tiger's name is \(myTiger.name) and its' breed is \(myTiger.breed) and the image is \(myTiger.image)")
         
@@ -50,6 +56,7 @@ class ViewController: UIViewController {
         fourthTiger.age = 5
         fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
         
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,6 +65,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        var randomIndex:Int
+        
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        } while currentIndex == randomIndex
+        
+        currentIndex = randomIndex
+        
+        let tiger = myTigers[randomIndex]
+        tiger.chuffANumberOfTimes(Int(arc4random_uniform(UInt32(myTigers.count))))
+//        myImageView.image = tiger.image
+//        nameLabel.text = tiger.name
+//        ageLabel.text = "\(tiger.age)"
+//        breedLabel.text = tiger.breed
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+                self.myImageView.image = tiger.image
+                self.nameLabel.text = tiger.name
+                self.ageLabel.text = "\(tiger.age)"
+                self.breedLabel.text = tiger.breed
+            }, completion: {
+                (finished: Bool) -> () in
+        })
+        
     }
 
 }
